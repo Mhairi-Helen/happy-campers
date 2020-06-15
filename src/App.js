f1c27fd4a16721d5f30fceef150d4dde42c3fb61
Daniel Norris <dan.norris@hotmail.com>
2020-06-15 13:01:41 +0100

    basic routing setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import './App.css';
 
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+import Form from './components/Form/Form';
+import GET from './components/GET/GET';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Form />
+                    </Route>
+
+                    <Route exact path="/campsites">
+                        <GET></GET>
+                    </Route>
+
+                    <p>404 error. Site not found. </p>
+
+                </Switch>
+            </Router>
+
+        </div>
+    );
 }
 
 export default App;
