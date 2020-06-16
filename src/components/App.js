84ad46fe4f5e1ddd1ef10a016137f4263fc01af3
Mhairi Hislop <mhairihislop@Mhairis-MacBook-Pro.local>
2020-06-16 10:48:20 +0100

    merging changes in App.js

---
+++ src/components/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import {
+    Router,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+import Header from './Header';
+import Card from './Card';
+//import Campsite from './Campsite';
+//import Footer from './Footer';
+import Hero from './Hero';
+
+
+/* import history for programmatic navigation */
+import history from '../history';
+
+const App = () => {
+    return (
+        <div className="App">
+
+            <Router history={history}>
+
+                <h1>Happy Campers</h1>
+
+                <Switch>
+                    <Header />
+
+
+                    {/* <Route exact path="/campsites/:id" render={({ match }) => (
+                        <Campsite id={match.params.id} />
+                    )}>
+                    </Route> */}
+
+                    <Route exact path="/components">
+                        {/**
+                         * Components and style guide development go here
+                         *
+                         * e.g.
+                         *
+                         * <Button />
+                         * <ListGroup>
+                         *      <ListItem />
+                         * </ListGroup>
+
+                         */}
+                    </Route>
+
+                    <p>404 error. Site not found. </p>
+
+                </Switch>
+
+                <p>Footer goes here. Copyright, 2020. </p>
+
+            </Router>
+
+        </div>
+    );
+}
+
+export default App;
