bec0652719ff048201411e29ca9f6248b62bfcef
Mhairi Hislop <mhairihislop@Mhairis-MacBook-Pro.local>
2020-06-16 11:16:08 +0100

    adding styling and fonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './index.css';
-// import './scss/main.scss';
+import './assets/style/main.css';
 
 // import redux
 import { createStore, applyMiddleware, compose } from 'redux';
