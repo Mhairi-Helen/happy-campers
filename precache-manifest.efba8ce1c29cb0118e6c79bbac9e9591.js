442e155de0a048438186fee7c4440e67372deb7c
Harriet Goddard <goddardhe@gmail.com>
2020-06-18 17:14:05 +0100

    Updates

---
+++ precache-manifest.efba8ce1c29cb0118e6c79bbac9e9591.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f1dfd4a03bc0112f1ab74dee111a5b",
+    "url": "/happy-campers/index.html"
+  },
+  {
+    "revision": "8148d27e4de3cf37922f",
+    "url": "/happy-campers/static/css/main.dda47a76.chunk.css"
+  },
+  {
+    "revision": "d89ac3d87e6523ccae0a",
+    "url": "/happy-campers/static/js/2.24fbbb7a.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/happy-campers/static/js/2.24fbbb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8148d27e4de3cf37922f",
+    "url": "/happy-campers/static/js/main.77bc2fd0.chunk.js"
+  },
+  {
+    "revision": "70bbca20cf3ec32b7c00",
+    "url": "/happy-campers/static/js/runtime-main.80021b30.js"
+  },
+  {
+    "revision": "84f7328b5896ebb594a8d5559e39f41c",
+    "url": "/happy-campers/static/media/113.84f7328b.jpg"
+  },
+  {
+    "revision": "524a4cbdf53cdb9bb2742d2aa7a25c36",
+    "url": "/happy-campers/static/media/ArchivoBlack-Regular.524a4cbd.ttf"
+  },
+  {
+    "revision": "7130fdb0a3f94088119aa0f96db9b08b",
+    "url": "/happy-campers/static/media/Barlow-Bold.7130fdb0.ttf"
+  },
+  {
+    "revision": "865a0a175c66fd38550835b6c137d8a9",
+    "url": "/happy-campers/static/media/Barlow-Light.865a0a17.ttf"
+  },
+  {
+    "revision": "788f9cc4e37cde7847cd42d30bee07b7",
+    "url": "/happy-campers/static/media/Barlow-Medium.788f9cc4.ttf"
+  },
+  {
+    "revision": "2cce8c806c2d2e03adc2b239ae316b76",
+    "url": "/happy-campers/static/media/Barlow-Regular.2cce8c80.ttf"
+  },
+  {
+    "revision": "f568f29616d99885b4a8d0e84a08eb29",
+    "url": "/happy-campers/static/media/CaveatBrush-Regular.f568f296.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/happy-campers/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/happy-campers/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/happy-campers/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/happy-campers/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/happy-campers/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "8bad87734ac6c834243fa4eb96cc1fe1",
+    "url": "/happy-campers/static/media/angle-down-solid.8bad8773.svg"
+  },
+  {
+    "revision": "762a8d1dcb8865f4872fa5740ede856f",
+    "url": "/happy-campers/static/media/campfire.762a8d1d.jpg"
+  },
+  {
+    "revision": "22a1fffc7cd5d88249a92c2392802224",
+    "url": "/happy-campers/static/media/campground-solid.22a1fffc.svg"
+  },
+  {
+    "revision": "2f85eb9d46ceb2545fa2ea38ee1e48ff",
+    "url": "/happy-campers/static/media/facebook-f-brands.2f85eb9d.svg"
+  },
+  {
+    "revision": "477e9e32113a634c2e336dc5bbcf8c2c",
+    "url": "/happy-campers/static/media/globe-solid.477e9e32.svg"
+  },
+  {
+    "revision": "51eaeb317c5abfe24abcd943cf239cf4",
+    "url": "/happy-campers/static/media/instagram-brands.51eaeb31.svg"
+  },
+  {
+    "revision": "f004e825dddcfc6a610b96cf1939daf0",
+    "url": "/happy-campers/static/media/sign-in-alt-solid.f004e825.svg"
+  },
+  {
+    "revision": "20361df3fb57ab1cdc47a1264a37579c",
+    "url": "/happy-campers/static/media/twitter-brands.20361df3.svg"
+  },
+  {
+    "revision": "34c706452209b5dc43ee74ad3b87cbf2",
+    "url": "/happy-campers/static/media/user.34c70645.svg"
+  }
+]);
